1b2f277006cbabc5a13020f855b09501f81d72a8
tajzyy <travisjadam1@gmail.com>
2023-11-29 20:33:32 -0500

    UI is somewhat complete

--- client.py
+++ client.py
@@ -1,65 +1,146 @@
 import socket
 import threading
-import tkinter
+import tkinter as tk
+from tkinter.font import Font
+from tkinter import scrolledtext
+from tkinter import messagebox
+
+# Creation of the UI
+root = tk.Tk()
+root.geometry("550x600")
+root.title("Chat Application")
+root.resizable(False, False)
+
+# Fonts and colors for the UI
+WHITE = "#ffffff"
+DARK_GRAY = "#2d3142"
+GRAY = "#bfc0c0"
+ORANGE = "#ef8354"
+DARK_BLUE = "#4f5d75"
+FONT = Font(family="Comfortaa", size=20)
+BUTTON_FONT = Font(family="Comfortaa", size=14)
+SMALL_FONT = Font(family="Comfortaa", size=13)
 
 HOST = '127.0.0.1'
-PORT = 4444
+PORT = 3333
+
+# Socket class object
+# AF_INET is for IPv4
+# SOCK_STREAM is for TCP protocol (preferred for an application like this)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def add_message(message):
+    message_box.config(state=tk.NORMAL)
+    message_box.insert(tk.END, message + '\n')
+    message_box.config(state=tk.NORMAL)
+
+
+def connect():
+    try:
+        # Try connecting to the server
+        client.connect((HOST, PORT))
+        print("Connection successful")
+        add_message("[SERVER] Connection successful")
+
+        # Send the client username to the server
+        username = username_tb.get()
+        if username != '':
+            client.send(username.encode())
+        else:
+            messagebox.showerror("Invalid username", "Empty is not a valid username")
+            return
+
+        threading.Thread(target=listener, args=(client,)).start()
+
+        # Disable UI elements after successful connection
+        username_tb.config(state=tk.DISABLED)
+        join_button.config(state=tk.DISABLED)
+    except Exception as e:
+        messagebox.showerror("Unable to connect to server", f"Unable to connect {HOST} {PORT}. Error: {str(e)}")
+
+
+def send():
+    message = message_tb.get()
+    if message != '':
+        client.sendall(message.encode())
+        message_tb.delete(0, len(message))
+    else:
+        messagebox.showerror("Error", "Empty message")
+
+
+# Row configuration for specific width and height of frames
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=4)
+root.grid_rowconfigure(2, weight=1)
+
+# Customization of the UI
+top_frame = tk.Frame(root, width=550, height=100, bg=GRAY)
+top_frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+middle_frame = tk.Frame(root, width=550, height=400, bg=GRAY)
+middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
+
+bottom_frame = tk.Frame(root, width=550, height=100, bg=GRAY)
+bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
+
+username_label = tk.Label(top_frame, text="Enter Username:", font=FONT, bg=GRAY, fg=DARK_BLUE)
+username_label.pack(side=tk.LEFT, padx=10)
+
+username_tb = tk.Entry(top_frame, font=SMALL_FONT, bg=WHITE, fg=DARK_BLUE, width=25)
+username_tb.pack(side=tk.LEFT)
+
+join_button = tk.Button(top_frame, text="Join Server", font=BUTTON_FONT, highlightbackground=ORANGE,
+                        bg=DARK_BLUE, fg=DARK_GRAY, command=connect)
+join_button.pack(side=tk.LEFT, padx=15)
+
+message_tb = tk.Entry(bottom_frame, font=SMALL_FONT, bg=WHITE, fg=DARK_BLUE, width=48)
+message_tb.pack(side=tk.LEFT, padx=10)
+
+message_button = tk.Button(bottom_frame, text="Send", font=BUTTON_FONT, highlightbackground=ORANGE,
+                           bg=DARK_BLUE, fg=DARK_GRAY, command=send)
+message_button.pack(side=tk.LEFT, padx=5)
+
+message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=ORANGE, fg=DARK_BLUE,
+                                        width=76, height=23)
+message_box.config(state=tk.DISABLED)
+message_box.pack(side=tk.TOP)
 
 
 # Function that will listen for messages from the server
 def listener(client):
     while 1:
-        msg = client.recv(2048).decode('utf-8')
-        if msg != '':
-            username = msg.split("->")[0]
-            message = msg.split("->")[1]
-            print(f"[{username}] {message}")
-        else:
-            print(f"Message received from server is empty")
+        try:
+            msg = client.recv(2048).decode('utf-8')
+            if not msg:
+                print("Server disconnected.")
+                messagebox.showinfo("Disconnected", "Disconnected from the server.")
+                break
+            if msg.startswith("[SERVER]"):
+                add_message(msg)
+            else:
+                parts = msg.split("->")
+                if len(parts) >= 2:
+                    username = parts[0]
+                    message = '->'.join(parts[1:])
+                    add_message(f"[{username}] {message}")
+                else:
+                    print("Invalid message format received:", msg)
+        except socket.error as e:
+            print(f"Error reading data: {e}")
+            messagebox.showerror("Error", f"Error reading data from the server: {e}")
+            break
 
 
-# Function that will communicate with the server
-def communicate(client):
-    # Send the client username to the server
-    username = input("Please enter your username-> ")
-    if username != '':
-        client.send(username.encode())
-    else:
-        print("Empty is not a valid username")
-        exit(0)
-
-    threading.Thread(target=listener, args=(client, )).start()
-
-    send_message(client)
-
-
-# Function for sending messages to server
-def send_message(client):
-    while 1:
-        msg = input("Message-> ")
-        if msg != '':
-            client.sendall(msg.encode())
-        else:
-            print("Empty message")
-            exit(0)
 
 
 # Main function for client
 def main():
-    # Socket class object
-    # AF_INET is for IPv4
-    # SOCK_STREAM is for TCP protocol (preferred for an application like this)
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Try connecting to the server
     try:
-        # Providing host IP and port for server
-        client.connect((HOST, PORT))
-        print("Connection successful")
-    except:
-        print(f"Unable to connect {HOST} {PORT}")
-
-    communicate(client)
+        root.mainloop()
+    except KeyboardInterrupt:
+        print("Exiting the chat application.")
+        client.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -2,76 +2,78 @@ import socket
 import threading
 
 HOST = '127.0.0.1'
-PORT = 4444
+PORT = 3333
 LIMIT = 4
-# List of active clients
 active_clients = []
+server_lock = threading.Lock()
 
 
-# Function listens for messages from client
 def listener(username, client):
-    while 1:
-        # Decode the message sent from client
-        message = client.recv(2048).decode('utf-8')
-        if message != '':
-            # Formatting the message
-            msg = username + '->' + message
-            global_message(msg)
+    while True:
+        try:
+            message = client.recv(2048).decode('utf-8')
+            if not message:
+                print(f"{username} disconnected.")
+                prompt_message = "[SERVER]" + f"{username} left the chat"
+                with server_lock:
+                    global_message(prompt_message)
+                    active_clients.remove((username, client))
+                break
+            else:
+                msg = username + '->' + message
+                with server_lock:
+                    global_message(msg)
+        except socket.error as e:
+            print(f"Error reading data from {username}: {e}")
+            with server_lock:
+                active_clients.remove((username, client))
+            break
 
-        else:
-            print(f"Empty message from {username}")
 
-
-# Function to handle the client
 def client_h(client):
-    # Username to server
-    while 1:
-        # Decode the message sent from client
+    try:
         username = client.recv(2048).decode('utf-8')
         if username != '':
-            # Add username and client object to active_clients
-            # if there is a valid username
-            active_clients.append((username, client))
-            prompt_message = "Server->" + f"{username} joined the chat"
-            global_message(prompt_message)
-            break
+            with server_lock:
+                active_clients.append((username, client))
+                prompt_message = "[SERVER]" + f"{username} joined the chat"
+                global_message(prompt_message)
         else:
             print("Username is empty")
 
-    threading.Thread(target=listener, args=(username, client,)).start()
+        threading.Thread(target=listener, args=(username, client,)).start()
+    except Exception as e:
+        print(f"Error handling client: {str(e)}")
+        client.close()
 
 
-# Function that sends message to all connected clients
 def global_message(message):
     for user in active_clients:
         client_message(user[1], message)
 
 
-# Function to send message to specific user
 def client_message(client, message):
-    client.sendall(message.encode())
+    try:
+        client.sendall(message.encode())
+    except Exception as e:
+        print(f"Error sending message to client: {str(e)}")
+        with server_lock:
+            active_clients.remove((username, client))
+        client.close()
 
 
-# Main function for server
 def main():
-    # Socket class object
-    # AF_INET is for IPv4
-    # SOCK_STREAM is for TCP protocol (preferred for an application like this)
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Try catch for connecting to server
     try:
-        # Providing host IP and port for server
         server.bind((HOST, PORT))
         print(f"Server running on {HOST} {PORT}")
-    except:
-        print(f"Unable to connect to host: {HOST} and port {PORT}")
+    except Exception as e:
+        print(f"Unable to connect to host: {HOST} and port {PORT}. Error: {str(e)}")
 
-    # Server limit
     server.listen(LIMIT)
 
-    # While loop keeps listening for client connections
-    while 1:
+    while True:
         client, address = server.accept()
         print(f"Connection successful {address[0]} {address[1]}")
 
